32bf2dc4afdfb751aa64a339f43130cc18f58a3e
Cristian Pérez Juárez <cristianpj0808@gmail.com>
2025-12-26 14:36:51 +0100

    Create: Back-end structure

---
+++ back-end/cataleg-productes/src/main/java/ifc33b/dwesc/cataleg_productes/CatalegProductesApplication.java
@@ -0,0 +1,13 @@
+package ifc33b.dwesc.cataleg_productes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalegProductesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalegProductesApplication.class, args);
+	}
+
+}

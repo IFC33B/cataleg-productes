86e198364551ec20da2da9ee50dec409be6f9c1f
Cristian Pérez Juárez <cristianpj0808@gmail.com>
2025-12-26 15:43:51 +0100

    Add(ProducteRepository): add filtrarPerPreu query

--- back-end/cataleg-productes/src/main/java/ifc33b/dwesc/cataleg_productes/repository/ProducteRepository.java
+++ back-end/cataleg-productes/src/main/java/ifc33b/dwesc/cataleg_productes/repository/ProducteRepository.java
@@ -1,9 +1,17 @@
 package ifc33b.dwesc.cataleg_productes.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import ifc33b.dwesc.cataleg_productes.model.Producte;
 
 @Repository
-public interface ProducteRepository extends JpaRepository<Producte, Long> {}
+public interface ProducteRepository extends JpaRepository<Producte, Long> {
+    @Query("SELECT p FROM Producte p WHERE (:minPreu IS NULL OR p.preu >= :minPreu) AND (:maxPreu IS NULL OR p.preu <= :maxPreu)")
+    List<Producte> filtrarPerPreu(@Param("minPreu") Double minPreu, @Param("maxPreu") Double maxPreu);
+
+}

17de2727e35ee3eb2d0dffa50856ad8a68ba7a9b
Cristian Pérez Juárez <cristianpj0808@gmail.com>
2025-12-27 15:20:27 +0100

    Add(producte.service.ts): handleError method

--- front-end/cataleg-productes/src/app/services/producte.service.ts
+++ front-end/cataleg-productes/src/app/services/producte.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 import { Producte } from '../models';
 
 @Injectable({
@@ -9,7 +9,7 @@ import { Producte } from '../models';
 export class ProducteService {
   private apiURL = 'http://localhost:8080/api/productes'
 
-  constructor(private http: HttpClient) {};
+  constructor(private http: HttpClient) { };
 
   // Obtener todos los productos
   getAllProductes(): Observable<Producte> {
@@ -17,5 +17,25 @@ export class ProducteService {
       .pipe(
         catchError(this.handleError)
       )
+  }
+
+  // Gestión de errores
+    private handleError(error: HttpErrorResponse) {
+    let errorMessage = 'Error desconocido';
+  
+    if (error.error instanceof ErrorEvent) {
+      // Error del cliente
+      errorMessage = `Error: ${error.error.message}`
+    } else {
+      // Error del servidor
+      errorMessage = `Codigo de error: ${error.status}, Mensaje: ${error.message}`;
+    }
+  
+    console.log(errorMessage);
+    return throwError(() => ({
+      status: error.status,
+      message: errorMessage,
+      error: error.error
+    }))
   }
 }

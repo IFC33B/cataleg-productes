9c67f893b3a220dbc79f7ba312366b9214c5d987
Cristian Pérez Juárez <cristianpj0808@gmail.com>
2025-12-27 15:47:29 +0100

    Add(app.routes.ts): Routes

--- front-end/cataleg-productes/src/app/app.routes.ts
+++ front-end/cataleg-productes/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LlistaProductes } from './components/llista-productes/llista-productes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/productes', pathMatch: 'full' },
+    { path: 'productes', component: LlistaProductes },
+    // { path: 'formulari', component: FormulariProductes }
+];

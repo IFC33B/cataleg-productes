67640da454a6758d81b690a233ec6fc28196cbb6
Cristian Pérez Juárez <cristianpj0808@gmail.com>
2025-12-27 16:52:05 +0100

    Update(formulari-productes component): add reactive forms

--- front-end/cataleg-productes/src/app/components/formulari-productes/formulari-productes.ts
+++ front-end/cataleg-productes/src/app/components/formulari-productes/formulari-productes.ts
@@ -2,11 +2,11 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { Component, signal } from '@angular/core';
 import { ProducteService } from '../../services/producte.service';
 import { ProducteRequest } from '../../models';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulari-productes',
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './formulari-productes.html',
   styleUrl: './formulari-productes.css',
 })
@@ -14,36 +14,42 @@ export class FormulariProductes {
   carregant = signal(false);
   error = signal<HttpErrorResponse | null>(null);
 
-  // datos del formulario
-  nom: string = '';
-  preu?: number;
-  stock?: number;
-  imatgeUrl?: string;
+  // Formulario reactivo
+  producteForm!: FormGroup;
 
-  constructor(private producteService: ProducteService) {}
+  constructor(private producteService: ProducteService) { }
+
+  ngOnInit(): void {
+    this.producteForm = new FormGroup({
+      nom: new FormControl('', [Validators.required, Validators.minLength(4)]),
+      preu: new FormControl(null, [Validators.required, Validators.min(0.01)]),
+      stock: new FormControl(null, [Validators.required, Validators.min(0)]),
+      imatgeUrl: new FormControl('')
+    })
+  }
 
   // Añadir productos
-  afegirProducte(form: any) {
+  afegirProducte() {
     this.carregant.set(true);
     this.error.set(null);
 
-    // Request
-    const request: ProducteRequest = {nom: this.nom, preu: this.preu!, imatgeUrl: this.imatgeUrl, stock: this.stock!}
-
-    // Service
-    this.producteService.createProducte(request).subscribe({
-      next: (data) => {
-        this.carregant.set(false);
-      },
-
-      error: (err) => {
-        this.error.set(err);
-        this.carregant.set(false);
-        console.log(err);
-      }
-    })
-
-    form.resetForm()
+    if (this.producteForm.valid) {
+      // Service
+      this.producteService.createProducte(this.producteForm.value).subscribe({
+        next: (data) => {
+          this.carregant.set(false);
+          this.producteForm.reset()
+        },
+  
+        error: (err) => {
+          this.error.set(err);
+          this.carregant.set(false);
+          console.log(err);
+        }
+      })
+    } else {
+      alert("Datos inválidos")
+    }
   }
 
   // Recargar página
